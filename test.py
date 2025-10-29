3e1fd444e393ac4cd434d93f19acfc89d36976fa
Vicente <vlagosfulla@gmail.com>
2025-10-29 09:45:31 -0300

    agrego codigo python

---
+++ test.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self, name, last_name, age):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+
+    def login(self, name, last_name, age):
+        if self.name == name and self.last_name == last_name and self.age == age:
+            print('Acceso concedido')
+
+        else: 
+            print('Acceso denegado')
+
+user1 = User("Vicente", "Lagos", 25)
+user1.login("Vicente", "Lagos", 25) 
+user1.login("Ana", "Pérez", 30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
